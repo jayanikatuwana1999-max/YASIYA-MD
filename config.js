f928f9bd26bf34eb94d82664a1e4db92c22f3c76
jayanikatuwana1999-max <jayanikatuwana1999@gmail.com>
2025-10-24 06:53:35 +0530

    Update config.js

--- config.js
+++ config.js
@@ -8,5 +8,5 @@ function convertToBool(text, fault = 'true') {
 }
 
 module.exports = {
-    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?LPg4xB'
+    SESSION_ID: process.env.SESSION_ID || 'YASIYA-MD?8mLu4d'
 };
